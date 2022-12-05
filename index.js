5112ac2b6b89940e25756164c7fa8356d2a180d4
chiurly <chiurly@gmail.com>
2022-12-05 21:32:27 +0200

    Add products routing

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-import express from 'express';
-
 const PORT = 3000;
+
+import express from 'express';
 
 const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', function (request, response) {
-    response.send('Hello World!');
+    response.sendFile('public/index.html', { root: '.'});
+});
+
+app.get('/products', function (request, response) {
+    response.json({
+        "products": ["Kebabas", "Mėsainis", "Ledai", "Vanduo", "Coca-Cola"]
+    });
 });
 
 app.listen(PORT, function () {
