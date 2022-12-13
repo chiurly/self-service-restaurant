b3db9d1ce757bb9530b4e199c346f7eceec9d7c3
chiurly <63109177+chiurly@users.noreply.github.com>
2022-12-13 18:26:34 +0200

    Create orders API route

---
+++ routes/api/orders.js
@@ -0,0 +1,72 @@
+import express from 'express';
+import Order from '../models/Order.js';
+
+const router = express.Router();
+
+const getOrder = async (request, response, next) => {
+	let order;
+	try {
+		order = await Order.findById(request.params.id);
+		if (order == null) {
+			return response.status(404).json({ message: 'Cannot find order' });
+		}
+	} catch (error) {
+		response.status(500).json({ message: error.message });
+	}
+	response.order = order;
+	next();
+}
+
+router.get('/', async (request, response) => {
+	try {
+		const orders = await Order.find();
+		response.json(orders);
+	} catch (error) {
+		response.status(500).json({ message: error.message });
+	}
+});
+
+router.get('/:id', getOrder, (request, response) => {
+	response.json(response.order);
+});
+
+router.post('/', async (request, response) => {
+	const order = new Order({
+		products: [request.body.products]
+	});
+	try {
+		const newOrder = await order.save();
+		response.status(201).json(newOrder);
+	} catch (error) {
+		response.status(400).json({ message: error.message });
+	}
+});
+
+router.patch('/:id', getOrder, async (request, response) => {
+	if (request.body.dateCreated != null) {
+		response.order.dateCreated = request.body.dateCreated;
+	}
+	if (request.body.dateCompleted != null) {
+		response.order.dateCompleted = request.body.dateCompleted;
+	}
+	if (request.body.products != null) {
+		response.order.products = request.body.products;
+	}
+	try {
+		const updatedOrder = await response.order.save();
+		response.json(updatedOrder);
+	} catch (error) {
+		response.status(400).json({ message: error.message });
+	}
+});
+
+router.delete('/:id', getOrder, async (request, response) => {
+	try {
+		await response.order.remove();
+		response.json({ message: 'Deleted order' });
+	} catch (error) {
+		response.status(500).json({ message: error.message });
+	}
+});
+
+export default router;
